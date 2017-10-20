acdca65a1b0bf357b3ace3bea6aaac7cbbc3af56
Yosevu Kilonzo <yosevuk@gmail.com>
2017-10-20 17:41:55 -0500

    Setup Travis

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Magic Squares
+# [Magic Squares](https://kamusi.surge.sh)
+
+[![Build Status](https://travis-ci.org/bantuist/magic-squares.svg?branch=master)](https://travis-ci.org/bantuist/magic-squares)
--- src/components/App.test.js
+++ src/components/App.test.js
@@ -6,7 +6,6 @@ describe('App', () => {
   const app = shallow(<App />);
 
   it('renders without crashing', () => {
-    // const div = document.createElement('div');
     expect(app).toMatchSnapshot();
   });
 
