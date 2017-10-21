430670768e756d382da937a4682ebd8587d9cc67
Yosevu Kilonzo <yosevuk@gmail.com>
2017-10-20 20:41:05 -0500

    Compute cell coordinates

--- src/components/MagicSquares.js
+++ src/components/MagicSquares.js
@@ -9,9 +9,15 @@ const Grid = styled.div`
   grid-gap: 5px;
   grid-template-columns: repeat(${({gridSize}) => gridSize}, 100px);
 `;
-const Cell = styled.input`
+
+const Cell = styled.div`
+border: 1px solid #ddd;
+`;
+
+const Input = styled.input`
   height: 100px;
-  font-size: 4em;
+  width: 100%;
+  font-size: 1em;
   text-align: center;
   &:focus {
     outline: none;
@@ -20,14 +26,49 @@ const Cell = styled.input`
 
 export class MagicSquares extends Component {
   renderGrid(gridSize) {
+    const cell = { x: gridSize + 1, y: 0 };
+
     return Array.from(Array(gridSize * gridSize)).map((row, i) => {
+      // Move to next row (x--) and reset column (y = 0) at the end of each row
+      if (i % gridSize === 0) {
+        cell.x--;
+        cell.y = 0;
+      }
+      
+      // Move to next column (y++)
+      cell.y++;
+      
+      // Calculate diagonals based on the number of cells between diagonal cells
+      if (i % (gridSize + 1) === 0) {
+        cell.diagonal = '1';
+      } else if (i % (gridSize - 1) === 0) {
+        cell.diagonal = '2';
+      } else {
+        cell.diagonal = false;
+      }
+
+      let rowTotal, columnTotal;
+      if (cell.y === gridSize) rowTotal = true;
+      if (cell.x === 1) columnTotal = true;
+
+      // console.log(cell);
+
       return (
-        <Cell className="grid-cell" key={uuid()} placeholder={i + 1} />
+        <Cell className="grid-cell" key={uuid()}>
+          <Input 
+            data-cell={JSON.stringify(cell)} 
+            placeholder={`${++i}: (${cell.x}, ${cell.y})`} 
+          />
+          {(rowTotal || columnTotal) && 
+            <span>total: </span>
+          }
+        </Cell>
       );
     });
   }
   render() {
     const { gridSize } = this.props;
+    // const gridSize = 5;
 
     return (
       <Grid gridSize={gridSize}>
